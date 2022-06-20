63b21ccbbecafc692eb587e2f821e79772befc4b
REY DAVID <reydavid@enizoni.com>
2022-06-20 09:55:15 -0500

    Uso de procedimientos para Operar

---
+++ Ejecuciones/Movimientos.sql
@@ -0,0 +1,12 @@
+
+
+/*Configuración Alertas*/
+select * from Alerta;
+/*Cuanto Tenemos en Cartera*/
+EXEC Obten_Balance
+/*Valuación de monedas*/
+EXEC Obten_Tarjeta
+/*
+EXEC [Post_Compra_MXN_XRP] @CantidadXRP =10, @PrecioMN = 6.3;
+EXEC [Post_Venta_XRP_MXN]  @CantidadXRP =1, @PrecioMN = 6.7;
+*/
